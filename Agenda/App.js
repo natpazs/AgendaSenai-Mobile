a55a85f0f266b764f63cec73043d74cee68fd992
natpazs <nataliapazsa@gmail.com>
2024-06-13 16:19:24 -0300

    Co-authored-by: Arthuzinho <Arthurzinho02@users.noreply.github.com>

---
+++ Agenda/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function App(){
+    return(
+        <View>
+            <Text>Oi</Text>
+            <StatusBar style="auto"/>
+        </View>
+
+    );
+}
--- Agenda/app/TelasIniciais/Welcoming.tsx
+++ Agenda/app/TelasIniciais/Welcoming.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView } from "react-native";
-import Botao from "@comp/botao";
-import { Link } from 'expo-router';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
 
-export default function Welcoming() {
-    return (
-            <View>
-                <Botao texto="Entrar" href="" />
-            </View>
-    );
-}
+const { width } = Dimensions.get('window');
+
+const AgendaSenai = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>AgendaSenai</Text>
+        <Text style={styles.description}>
+          é uma plataforma que otimiza a gestão de eventos e cursos do SENAI, oferecendo acesso fácil e em tempo real a horários e notificações.
+        </Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>ENTRAR</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  background: {
+    height: '60%',
+    position: 'relative',
+    backgroundColor: '#C21807',
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    overflow: 'hidden',
+
+  },
+  svg: {
+    position: 'absolute',
+    bottom: 0,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    marginTop: '40%',
+  },
+  title: {
+    color: '#C21807',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  description: {
+    color: '#000',
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#C21807',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default AgendaSenai;
